53a28474b8630682b381fd166e32b097686a0e29
Eduardo de Andrade <59586689+64651585@users.noreply.github.com>
2020-12-08 15:20:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ContadorNomes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Contador de Nomes ==========");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("O seu nome: {0}, possui {1} caracteres", nome, nome.Length);
+
+        }
+    }
+}
